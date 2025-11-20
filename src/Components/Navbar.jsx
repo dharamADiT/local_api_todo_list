7eca8149e7f522b3d080a92beb51654e17f62d67
dharamADIT <dharam892001@gmail.com>
2025-11-20 09:39:37 +0530

    commit for change user to - todo

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className='flex items-center   justify-center gap-10 py-6'>
         <NavLink className={(e)=>e.isActive? "text-red-400": ""} to="/">Home</NavLink>
-        <NavLink className={(e)=>e.isActive? "text-red-400": ""} to="/user-create">Create user</NavLink>
+        <NavLink className={(e)=>e.isActive? "text-red-400": ""} to="/user-create">Create To do</NavLink>
         {/* <NavLink className={(e)=>e.isActive? "text-red-400": ""} to="/update">Update</NavLink> */}
       
         </nav>
